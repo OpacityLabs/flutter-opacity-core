3a86dadd2c732695c2fb965ee769ed5fd8e6f0e6
Oscar Franco <ospfranco@gmail.com>
2025-02-05 17:52:22 +0100

    Update iOS

--- ios/Classes/FlutterOpacityCorePlugin.swift
+++ ios/Classes/FlutterOpacityCorePlugin.swift
@@ -2,63 +2,86 @@ import Flutter
 import OpacityCore
 
 public class FlutterOpacityCorePlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(
-      name: "flutter_opacity_core", binaryMessenger: registrar.messenger())
-    let instance = FlutterOpacityCorePlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "init":
-      if let args = call.arguments as? [String: Any],
-        let apiKey = args["apiKey"] as? String,
-        let dryRun = args["dryRun"] as? Bool,
-        let environment = args["environment"] as? Int
-      {
-        guard let environmentEnum = OpacitySwiftWrapper.Environment(rawValue: environment) else {
-          result(
-            FlutterError(
-              code: "INVALID_ENVIRONMENT", message: "Invalid environment value", details: nil))
-          return
-        }
-        do {
-          try OpacitySwiftWrapper.initialize(
-            apiKey: apiKey, dryRun: dryRun, environment: environmentEnum)
-          result(nil)
-        } catch {
-          result(
-            FlutterError(
-              code: "ERROR_INITIALIZING", message: error.localizedDescription, details: nil))
-        }
-      } else {
-        result(FlutterError(code: "INVALID_ARGUMENTS", message: "Invalid arguments", details: nil))
-      }
-
-    case "get":
-      if let args = call.arguments as? [String: Any],
-        let name = args["name"] as? String
-      {
-        let params = args["params"] as? [String: Any]
-        Task {
-          do {
-            let res = try await OpacitySwiftWrapper.get(
-              name: name,
-              params: params
-            )
-            result(res)
-          } catch {
-            result(
-              FlutterError(
-                code: "ERROR_GETTING_FLOW", message: error.localizedDescription, details: nil))
-          }
-        }
-      } else {
-        result(FlutterError(code: "INVALID_ARGUMENTS", message: "Invalid arguments", details: nil))
-      }
-    default:
-      result(FlutterMethodNotImplemented)
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(
+            name: "flutter_opacity_core", binaryMessenger: registrar.messenger()
+        )
+        let instance = FlutterOpacityCorePlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
+
+    public func handle(
+        _ call: FlutterMethodCall, result: @escaping FlutterResult
+    ) {
+        switch call.method {
+        case "init":
+            if let args = call.arguments as? [String: Any],
+                let apiKey = args["apiKey"] as? String,
+                let dryRun = args["dryRun"] as? Bool,
+                let environment = args["environment"] as? Int,
+                let shouldShowErrorsInWebView = args[
+                    "shouldShowErrorsInWebView"] as? Bool
+            {
+                guard
+                    let environmentEnum = OpacitySwiftWrapper.Environment(
+                        rawValue: environment)
+                else {
+                    result(
+                        FlutterError(
+                            code: "INVALID_ENVIRONMENT",
+                            message: "Invalid environment value", details: nil))
+                    
+                    return
+                }
+                
+                do {
+                    try OpacitySwiftWrapper.initialize(
+                        apiKey: apiKey, dryRun: dryRun,
+                        environment: environmentEnum,
+                        shouldShowErrorsInWebView: shouldShowErrorsInWebView)
+                    
+                    result(nil)
+                } catch {
+                    result(
+                        FlutterError(
+                            code: "ERROR_INITIALIZING",
+                            message: error.localizedDescription, details: nil))
+                }
+            } else {
+                result(
+                    FlutterError(
+                        code: "INVALID_ARGUMENTS", message: "Invalid arguments",
+                        details: nil))
+            }
+
+        case "get":
+            if let args = call.arguments as? [String: Any],
+                let name = args["name"] as? String
+            {
+                let params = args["params"] as? [String: Any]
+                Task {
+                    do {
+                        let res = try await OpacitySwiftWrapper.get(
+                            name: name,
+                            params: params
+                        )
+                        result(res)
+                    } catch {
+                        result(
+                            FlutterError(
+                                code: "ERROR_GETTING_FLOW",
+                                message: error.localizedDescription,
+                                details: nil))
+                    }
+                }
+            } else {
+                result(
+                    FlutterError(
+                        code: "INVALID_ARGUMENTS", message: "Invalid arguments",
+                        details: nil))
+            }
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
 }
